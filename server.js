51a754e94938daad90ac628917384dbbfa7f8597
gopiKishan <gopikishan6719@gmail.com>
2026-08-15 01:32:04 +0530

    Revert "completion of authentication code"
    This reverts commit 0c549a071ff90b06ab487545326771351af84347.

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import dotenv from'dotenv';
 dotenv.config();
-
-
 import app from './src/app.js';
 import connectToDB from './src/config/db.js';
 connectToDB()
--- src/app.js
+++ src/app.js
@@ -4,12 +4,6 @@ import express from "express";
 const app = express();
 
 app.use(express.json())
-/* importing all the routes here */
-import { authRouter } from "./routes/auth.routes.js";
-
-
-/*using all the routes here */
-app.use("/api/auth",authRouter)
 
 
 export default app
